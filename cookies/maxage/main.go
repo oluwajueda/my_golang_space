d8f3f08525dfe92742df2ebbc765afab6292f0ac
oluwajueda <oluwajuedaloade@gmail.com>
2023-12-04 11:41:17 +0100

    working on cookies

---
+++ cookies/maxage/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/set", set)
+	http.HandleFunc("/read", read)
+	http.HandleFunc("/expire", expire)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, `<h1><a href="/set">set a cookie</a></h1>`)
+}
+
+func set(w http.ResponseWriter, req *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "session",
+		Value: "some value",
+		Path:  "/",
+	})
+	fmt.Fprintln(w, `<h1><a href="/read">read</a></h1>`)
+}
+
+func expire(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/set", http.StatusSeeOther)
+		return
+	}
+	c.MaxAge = -1 // delete cookie
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
